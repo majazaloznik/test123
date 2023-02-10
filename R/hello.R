32fff4d8cfd058b3561cb70394de5da58b0cee05
majazaloznik <maja.zaloznik@gmail.com>
2023-02-10 15:44:49 +0100

    Passing tests and checks locally

--- R/hello.R
+++ R/hello.R
@@ -1,11 +1,28 @@
+#' Lookup fun
+#'
+#' @param x character string
+#'
+#' @return numeric code or error
+#' @export
+#'
 codes <- function(x){
   switch(x,
          "Test" = 1,
-         "Tešt" = 2,
-         "čšž" = 3,
+         "Te\u0161t" = 2,
+         "\u010d\u0161\u017e" = 3,
          stop(paste0("nope, isn't working for", x)))
 }
 
+#' Pipeline fun
+#'
+#' Reads excel table from file and uses the lookup function
+#' to recode the values in `V1`
+#'
+#' @param path path to excel file
+#'
+#' @return dataframe with added `code` variable.
+#' @export
+#'
 test <- function(path){
 
   df <- readxl::read_excel(path )
