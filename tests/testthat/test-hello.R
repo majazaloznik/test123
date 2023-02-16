6720976e236d0f0e4342f5bf2c68949e8c185892
majazaloznik <maja.zaloznik@gmail.com>
2023-02-16 16:43:50 +0100

    check encoding on runner

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -3,13 +3,7 @@ pth <- testthat::test_path("testdata", "test.xlsx")
 
 test_that("reading and switching", {
   df <- readxl::read_excel(pth)
-  cat("\ndeclared encodings with Encoding():`\n")
-  print(Encoding(df[[1]]))
-  cat("\ndetected encodings with stringi:`\n")
-  print(stringi::stri_enc_detect(df[[1]]))
-  cat("\ncheck is utf-8\n:")
-  print(stringi::stri_enc_isutf8(df[[1]]))
+  cat("\ncheck local encoding:")
+  print(l10n_info())
   expect_equal(test(pth)$code , c(1,2,3))
-  stringi::stri_enc_isutf8(df[[1]])
 })
-
