db969f6e648764a613d5391a785efeb8312a4739
majazaloznik <maja.zaloznik@gmail.com>
2023-02-16 11:41:42 +0100

    Add utf8 check

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -7,5 +7,9 @@ test_that("reading and switching", {
   print(Encoding(df[[1]]))
   cat("\ndetected encodings with stringi:`\n")
   print(stringi::stri_enc_detect(df[[1]]))
+  cat("\ncheck is utf-8\n:")
+  print(stringi::stri_enc_isutf8(df[[1]]))
   expect_equal(test(pth)$code , c(1,2,3))
+  stringi::stri_enc_isutf8(df[[1]])
 })
+
