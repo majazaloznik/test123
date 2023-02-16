717f91012e842a8dc454bc617d5b28d9a2be136e
majazaloznik <maja.zaloznik@gmail.com>
2023-02-16 09:39:56 +0100

    Add encoding checks

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -2,5 +2,10 @@ pth <- testthat::test_path("testdata", "test.xlsx")
 
 
 test_that("reading and switching", {
+  df <- readxl::read_excel(pth)
+  cat("\ndeclared encodings with Encoding():`\n")
+  print(Encoding(df[[1]]))
+  cat("\ndetected encodings with stringi:`\n")
+  print(stringi::stri_enc_detect(df[[1]]))
   expect_equal(test(pth)$code , c(1,2,3))
 })
